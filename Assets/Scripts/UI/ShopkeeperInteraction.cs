c2144369fee4849557ea45a04464a6fb01fce970
Dainis K <bonanzaa@inbox.lv>
2021-01-10 14:36:18 +0100

    Added basic shop layout as well as a shopkeeper placeholder

---
+++ Assets/Scripts/UI/ShopkeeperInteraction.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopkeeperInteraction : MonoBehaviour
+{
+    public float _interactionRange = 5f;
+    public Transform _player;
+    public GameObject shopUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Opens up UI when detects a player in a range, and closes the window, when 
+        // player gets too far
+        
+        if (Vector3.Distance(_player.position, transform.position) <= _interactionRange)
+        {
+            shopUI.SetActive(true);
+        } else
+        {
+            shopUI.SetActive(false);
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // visualisation of a detection range
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _interactionRange);
+    }
+
+   
+}
