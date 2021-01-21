46d94ae163f865a96f065c06f3e39201afcc0580
Pete <pete.kolpakov@gmail.com>
2021-01-21 15:57:28 +0100

    polished/added functionality to Entity Base Class
    Added DealDamage and SetNewMaxHealth functions

--- Assets/Scripts/EntityClass/Entity.cs
+++ Assets/Scripts/EntityClass/Entity.cs
@@ -8,10 +8,12 @@ public class Entity : MonoBehaviour
     public int health;
 
     [SerializeField]
-    private int currentHealth;
+    private int _maxHealth;
 
-    [SerializeField]
-    private int healthMax;
+    public void Initialize() // to be called at the beginning of a lvl
+    {
+        health = _maxHealth;
+    }
 
     public int GetHealth()
     {
@@ -21,6 +23,21 @@ public class Entity : MonoBehaviour
     public void Heal(int healAmount)
     {
         health += healAmount;
-        if (health > healthMax) health = healthMax;
+        if (health > _maxHealth) health = _maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        // TODO - take armor ponts into account
+        health -= damage;
+    }
+
+    public void SetNewMaxHealth(int newMax)
+    {
+        _maxHealth = newMax;
+        if(newMax <= 0)
+        {
+            Debug.LogError("WARNING: Max HP must be greater than zero!");
+        }
     }
 }
