363340791f80b259190c778c0c43e3c4c2f9ecde
Víctor Sánchez Martínez <visama396@gmail.com>
2021-01-24 14:43:16 +0100

    Adding skills

---
+++ Assets/Scripts/Companion/HealingSkill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Companion
+{
+    class HealingSkill : Skill
+    {
+        private Entity _playerEntity;
+        
+        public HealingSkill(GameObject player, Entity entity) : base(player)
+        {
+            _playerEntity = entity;
+        }
+
+        public override void Activate()
+        {
+        }
+
+        private IEnumerator HealPlayer()
+        {
+            yield return new WaitForSeconds(30f);
+            _playerEntity.Heal(10);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Companion/Skill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Companion
+{
+    abstract class Skill
+    {
+        private string _skillName;
+        private GameObject _player;
+
+        public Skill(GameObject player)
+        {
+            _player = player;
+        }
+
+        abstract public void Activate();
+
+        public string GetSkillName() => _skillName;
+
+        public void SetSkillName(string name) => _skillName = name;
+    }
+}
