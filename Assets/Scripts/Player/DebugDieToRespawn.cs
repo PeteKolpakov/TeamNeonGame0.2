2a86e26a5aad34dd67f37fede25518bcd4ae61bf
Pete <pete.kolpakov@gmail.com>
2021-02-19 20:13:33 +0100

    Added Respawn Debug to F8 key + Lvl design work

---
+++ Assets/Scripts/Player/DebugDieToRespawn.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.EntityClass;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    class DebugDieToRespawn : MonoBehaviour
+    {
+        private PlayerBase _player;
+        private void Start()
+        {
+            if (_player == null)
+                _player = GetComponent<PlayerBase>();
+            else _player = default;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F8))
+                _player.TakeDamage(float.MaxValue, DamageType.Fall);
+        }
+    }
+}
