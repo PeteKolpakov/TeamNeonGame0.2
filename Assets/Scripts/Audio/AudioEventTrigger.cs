eba267bdef42fd3579db165c3e70ccb9bc366ab2
Pete <pete.kolpakov@gmail.com>
2021-02-25 15:48:13 +0100

    Integrated FMOD
    implemented triggering OneShot clips from audio trigger script
    added audio test scene and sound trigger tester script that calls play audio on spacebar press to test audio

--- Assets/Scripts/Audio/AudioEventTrigger.cs
+++ Assets/Scripts/Audio/AudioEventTrigger.cs
@@ -5,8 +5,32 @@ using UnityEngine;
 
 namespace Assets.Scripts.Audio
 {
+    /// <summary>
+    /// Handles Triggering AudioClips through FMOD
+    /// </summary>
     class AudioEventTrigger : MonoBehaviour
     {
-        
+        [FMODUnity.EventRef] public string Event;
+        [SerializeField]     private bool PlayOnAwake;
+        [SerializeField]     private bool PlayOnDestroy;
+
+        /// <summary>
+        /// Plays Audio Clip selected in component
+        /// </summary>
+        public void PlayOneShot()
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached(Event, gameObject);
+        }
+
+        private void Start()
+        {
+            if (PlayOnAwake)
+                PlayOneShot();            
+        }
+        private void OnDestroy()
+        {
+            if (PlayOnDestroy)
+                PlayOneShot();
+        }
     }
 }
---
+++ Assets/Scripts/Misc/SoundTriggerTester.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Audio;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Misc
+{
+    class SoundTriggerTester : MonoBehaviour
+    {
+        private AudioEventTrigger _trigger;
+
+        private void Awake()
+        {
+            _trigger = GetComponent<AudioEventTrigger>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _trigger.PlayOneShot();
+            }
+        }
+
+    }
+}
