548582e6e8d85c2d9b5f82b0ba2870c014d00bd0
Pete <pete.kolpakov@gmail.com>
2021-02-28 17:43:21 +0100

    added more sounds

---
+++ Assets/Scripts/GameManager/BGMPlayer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.GameManager
+{
+    class BGMPlayer : MonoBehaviour
+    {
+        private static FMOD.Studio.EventInstance _music;
+
+        [SerializeField][FMODUnity.EventRef] private string _track;
+        [SerializeField][Range(0f, 1f)]      private float _defaultXFade = 0f;
+
+        private void Start()
+        {
+            _music = FMODUnity.RuntimeManager.CreateInstance(_track);
+            _music.setParameterByName("XFade", _defaultXFade);
+            _music.start();
+            _music.release();
+        }
+
+
+        private void Update()
+        {
+            // TODO remove this function
+            SetBGMXFade(_defaultXFade);
+        }
+
+        private void OnDestroy()
+        {
+            _music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+
+        public void SetBGMXFade(float value)
+        {
+            _music.setParameterByName("XFade", value);
+        }
+
+    }
+}
