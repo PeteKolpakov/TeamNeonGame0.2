8eece6a2af330aa2a4990360097cefc3548c596e
Daniel Salas <salasdaniel1998@gmail.com>
2021-02-28 11:09:13 +0100

    Applied changes to Player Prefab and after Image only while dashing

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,8 @@ namespace Assets.Scripts.Player
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField]
+        private ParticleSystem _dashAfterImage;
+        [SerializeField]
         [Range(1,10)]
         private float _dashLenght;
         [SerializeField]
@@ -81,7 +83,8 @@ namespace Assets.Scripts.Player
         }
 
         private void DashStart()
-        {       
+        {
+            _dashAfterImage.Play();
             Vector2 dashPosition = ((Vector2)transform.position + _moveDirection * _dashLenght);
             
             Vector2 dashVelocity = (dashPosition - (Vector2)transform.position) / _dashDuration;
