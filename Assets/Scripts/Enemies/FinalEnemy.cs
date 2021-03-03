9e862f9735fd41151362fce7277481b8fb86e141
Pete <pete.kolpakov@gmail.com>
2021-03-03 17:33:33 +0100

    Added Final Enemy Prefab, added game level

---
+++ Assets/Scripts/Enemies/FinalEnemy.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.GameManager;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Enemies
+{
+    class FinalEnemy : MonoBehaviour
+    {
+        private ScenesManager sceneManager;
+
+        private void Awake()
+        {
+            sceneManager = GameObject.Find("GameManager").GetComponent<ScenesManager>();
+        }
+        private void OnDestroy()
+        {
+            sceneManager.GoToNextLevel();
+        }
+
+    }
+}
