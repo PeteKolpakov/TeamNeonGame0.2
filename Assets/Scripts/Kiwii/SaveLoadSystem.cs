a1d6c3503b5e317c2425d4c2bd3894ce90d4f7cf
kiwii <ivanlopzv@gmail.com>
2021-03-03 19:47:30 +0100

    trash

--- Assets/Scripts/Kiwii/SaveLoadSystem.cs
+++ Assets/Scripts/Kiwii/SaveLoadSystem.cs
@@ -21,6 +21,8 @@ public class SaveLoadSystem : MonoBehaviour
     [SerializeField]
     private PlayerStatManager PStats;
 
+    [SerializeField]
+    private LevelManager LevelManagerCheck;
 
     private void Start()
     {
@@ -42,8 +44,9 @@ public class SaveLoadSystem : MonoBehaviour
         entity.maxHealth = MyCurrentPlayerData.PlayerMaxHealth;
         PStats._moneyAmount = MyCurrentPlayerData.PlayerCurrentMoney;
         PStats.LoadWeapons(MyCurrentPlayerData.PurchasedGunsInt);
+        LevelManagerCheck.LevelIsUnlocked = MyCurrentPlayerData.CompletedLevelsInt;
 
-        // This is being called and loads the data even if you dont want to. now that doesnt happen anymore for some reason
+        
 
 
 
@@ -59,7 +62,8 @@ public class SaveLoadSystem : MonoBehaviour
         p.PlayerMaxHealth = entity.maxHealth;
         p.PlayerCurrentMoney = PStats._moneyAmount;
         p.PurchasedGunsInt = PStats.BoughtGunsInt;
-   //     p.CompletedLevelsInt = PStats.FinishedLevelsInt;
+     
+        p.CompletedLevelsInt = LevelManagerCheck.LevelIsUnlocked;
 
         Debug.Log("Im trying to save weapons");
         Debug.Log(PStats.BoughtGunsInt.Count);
